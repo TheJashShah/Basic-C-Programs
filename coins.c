6641fab05d5f349e2a7a0b12bf5f0076ec4c3ad1
JashShah <78263590+TheJashShah@users.noreply.github.com>
2023-11-02 00:15:50 +0530

    Update coins.c

--- coins.c
+++ coins.c
@@ -2,7 +2,7 @@
 #include<math.h>
 
 /*
-Another interesting problem I came across. An entity is trapped  inside a grid of n rows and n columns. n is odd(always), with the center of the grid being (n+1/2, n+1/2). The entity is at a tile(x,y), with x and y both never being equal to the center. He must reach the center.
+Another interesting problem I came across. An entity is trapped inside a grid of n rows and n columns. n is odd(always), with the center of the grid being (n+1/2, n+1/2). The entity is at a tile(x,y), with x and y both never being equal to the center. He must reach the center.
 To move one tile horizontally, he has to spend 1 coin, same with traveling in the y-direction. Moving diagonally will not cost him any coin.
 Find the minimum number of coins he must spend to reach the center.
 
