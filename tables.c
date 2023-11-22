d345449ae610e52bbc445a8de7a957f0bce59e01
JashShah <78263590+TheJashShah@users.noreply.github.com>
2023-11-22 19:36:51 +0530

    Add files via upload

---
+++ tables.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void Table(int n, int arr[][n]){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            arr[i][j] = (i + 1)*(j + 1);
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf(" %2d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+
+    int n;
+    printf("Enter the highest number for the table: \n");
+    scanf("%d", &n);
+
+    int arr[n][n];
+    printf("Here is your table: \n");
+    Table(n, arr);
+
+    return 0;
+}
